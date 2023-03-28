1a4520d948675185bf88531d8a3b6f1b1aaabeb1
LUCA VINÍCIUS MESADRI <luca.mesadri@univille.br>
2023-03-28 20:18:54 -0300

    starting exercise11

---
+++ fluxograma2/exercise11.py
@@ -0,0 +1,14 @@
+# 11)Escreva o algoritmo em FLUXOGRAMA que calcule o valor total de aluguel e taxas a serem cobrados de um morador. 
+#    Solicite o valor do aluguel, o valor do condomínio, considere que o valor do metro cubico do gas é de R$ 1,80 
+#    e o valor da taxa de consumo de agua é de R$ 0,60 por metro cubico de agua consumida. 
+#    Apresente também o valor de 5% do valor total do aluguel que pode ser dado como desconto caso seja pago 5 dias antes do vencimento.
+
+print("Let's calculate how much is your total rent and taxes!")
+
+print("Type the value of your rent:")
+rent = int(input())
+
+print("Type the value of the condominium:")
+condominium = int(input())
+
+print
